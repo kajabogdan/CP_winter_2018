a8a4c9e95ef9387ca47b0990b86d2de551701e80
kajabogdan <kajuniab@gmail.com>
2018-10-17 13:05:35 +0200

    Testing Variables STRING

--- src/pl/waw/sgh/PlayWithText.java
+++ src/pl/waw/sgh/PlayWithText.java
@@ -14,13 +14,29 @@ public class PlayWithText {
         //Wyswietla dany znak w stringu liczac od 0:
         System.out.println(s1.charAt(2));
 
-        //
+        //ucina po 2 literce, liczac od 0
         System.out.println(s3.substring(2));
         System.out.println(s3.substring(1, 4));
 
         System.out.println(s1len);
 
+        String s4 = "ABC";
+        //String s5 = "ABC"
+        String s5 = new String("ABC");
+        System.out.println(s4);
+        System.out.println(s5);
 
+        /*if (s4== s5) {
+            System.out.println("s4 and s5 are equal");
+        } else {
+            System.out.println("s4 and s5 are not equal");
+            // nie rowna sie to samo, bo to rozne obiekty, a literek nie porownuje */
+
+        if (s4.equals(s5)) {
+            System.out.println("s4 and s5 are equal by content");
+        } else {
+            System.out.println("s4 and s5 are not equal by content");
+        }
     }
 }
 
