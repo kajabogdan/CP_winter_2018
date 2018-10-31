bcea5167dc1e8846ba500df4bdb894aa45443472
kajabogdan <kajuniab@gmail.com>
2018-10-31 10:42:46 +0100

    Loops

--- src/pl/waw/sgh/Blocks.java
+++ src/pl/waw/sgh/Blocks.java
@@ -1,6 +1,13 @@
 package pl.waw.sgh;
 
 public class Blocks {
+    //visible everywhere
+    public static int i0 = 5;
+    //for static variables behaves like public but only when it's in the same package! package = pl.waw.sgh
+    protected static int i3 = 5;
+    //private is only visible within one class
+    private int i4 = 5;
+
     public static void main(String[] args) {
         int i1 = 0;
         // when something is in a block it's not visible from the parent, you can't do it like this
