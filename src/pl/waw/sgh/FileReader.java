066067a8e797eda51baa6cf9234dd82f9061edc4
kajabogdan <kajuniab@gmail.com>
2018-11-07 13:08:27 +0100

    File reading

---
+++ src/pl/waw/sgh/FileReader.java
@@ -0,0 +1,20 @@
+package pl.waw.sgh;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inFile = new File("h://mytxtfile.txt");
+
+        Scanner scanner = new Scanner(inFile);
+        int i = 0;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(i + ": " + line);
+
+        }
+
+    }
+}
