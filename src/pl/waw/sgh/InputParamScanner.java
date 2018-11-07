33e120514de12d4c21db6d0082f26d499e535d7c
kajabogdan <kajuniab@gmail.com>
2018-11-07 10:33:16 +0100

    Scanner

---
+++ src/pl/waw/sgh/InputParamScanner.java
@@ -0,0 +1,26 @@
+package pl.waw.sgh;
+
+import java.util.Scanner;
+
+public class InputParamScanner {
+    public static void main(String[] args) {
+        System.out.println(" This is output");
+        System.err.println(" This is error");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please provide your name ");
+        // this one never breaks
+        while (scanner.hasNext()) {
+            String name = scanner.next();
+            if (name.equals("exit")) break;
+            System.out.println("Hello" + name);
+        }
+        // this doesn't accept something that is not an Int
+        while (scanner.hasNextInt()) {
+            Integer myInt = scanner.nextInt();
+            if (myInt.equals(0)) break;
+            System.out.println("I got " + myInt);
+
+        }
+    }
+}
